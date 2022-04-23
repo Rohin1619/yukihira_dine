3c5184e1785f444592965d6a06dcce14284effeb
Rohin Bharatee <rohinbharatee@gmail.com>
2022-04-23 23:58:16 +0545

    commit on 'master'

--- catalog/filters.py
+++ catalog/filters.py
@@ -1,9 +1,15 @@
-from pyexpat import model
 import django_filters
+from django_filters import DateFilter, CharFilter
 
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
-    class Meta:
-        model = Order
-        fields = '__all__'
+	start_date = DateFilter(field_name="date_created", lookup_expr='gte')
+	end_date = DateFilter(field_name="date_created", lookup_expr='lte')
+	note = CharFilter(field_name='note', lookup_expr='icontains')
+
+
+	class Meta:
+		model = Order
+		fields = '__all__'
+		exclude = ['table', 'date_created']
